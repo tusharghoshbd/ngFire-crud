3ff5608bff2f8313af7336a47275ca7efaa683bb
Tushar <tusharghosh09006@gmail.com>
2020-07-01 07:38:10 +0600

    login authentication implemented

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,29 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { AngularFireAuth } from "@angular/fire/auth";
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class AuthService {
-   
+
     constructor(
         public http: HttpClient,
-        public  afAuth:  AngularFireAuth,
+        public auth: AngularFireAuth,
         private fireStore: AngularFirestore) { }
-    
-    login(email: string, password: string) {
-        // return this.afAuth.auth.signInWithEmailAndPassword(email, password)
-        // .then((result) => {
-        //     this.ngZone.run(() => {
-        //     this.router.navigate(['dashboard']);
-        //     });
-        //     this.SetUserData(result.user);
-        // }).catch((error) => {
-        //     window.alert(error.message)
-        // })
+
+    signIn(email: string, password: string) {
+        console.log(email, password);
+        this.auth.signInWithEmailAndPassword(email, password)
+            .then((user) => {
+                // console.log(this.auth.currentUser);
+                console.log(user);
+            }).catch((error) => {
+                console.log(error);
+            })
     }
-    
-} 
+
+}
 
--- src/app/auth/sign-in/sign-in.component.ts
+++ src/app/auth/sign-in/sign-in.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../auth.service';
+import { AngularFireAuth } from "@angular/fire/auth";
+import { auth } from 'firebase/app';
 
 @Component({
-  selector: 'app-sign-in',
-  templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.css']
+    selector: 'app-sign-in',
+    templateUrl: './sign-in.component.html',
+    styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent implements OnInit {
 
-  constructor() { }
+    email: string="tusharghosh09006@gmail.com";
+    password: string="test1234";
 
-  ngOnInit(): void {
-  }
+    constructor(
+        private authService: AuthService,
+        public auth: AngularFireAuth) { }
+
+    ngOnInit(): void {}
+
+    signIn() {
+        // this.authService.signIn(this.email, this.password).then((user: any) => {
+
+        // }, err => {
+        //     console.log(err);
+        // });
+
+        this.auth.signInWithPopup(new auth.GoogleAuthProvider());
+    }
 
 }
