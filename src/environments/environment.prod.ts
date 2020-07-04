e5b0f2cef1281e4d85b6e961ff543de3029276b8
Tushar <tusharghosh09006@gmail.com>
2020-07-04 22:51:07 +0600

    deploy in firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+    production: true,
+    firebaseConfig : {
+        apiKey: "AIzaSyDJC4OqfcgUPibAko6JWpH7otoNKtjwAqA",
+        authDomain: "ngfire-crud-bc212.firebaseapp.com",
+        databaseURL: "https://ngfire-crud-bc212.firebaseio.com",
+        projectId: "ngfire-crud-bc212",
+        storageBucket: "ngfire-crud-bc212.appspot.com",
+        messagingSenderId: "1033738583344",
+        appId: "1:1033738583344:web:b8fd742c3349771b525cf5"
+    }
 };
